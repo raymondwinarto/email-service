ec9d09b0c8f98792d644d9b97271a09a69a024b6
Raymond Winarto <raymond.winarto@complispace.com.au>
2020-11-26 21:33:09 +1100

    further improve swagger documentation

--- src/lib/utils/joi-schemas.js
+++ src/lib/utils/joi-schemas.js
@@ -1,14 +1,20 @@
 const Joi = require('joi');
 
-const emailAddress = Joi.string().email().description('Email Address');
+const emailAddress = Joi.string().email().description('email address');
 
 const mail = Joi.object({
-  tos: Joi.array().items(emailAddress).required().label('Email Address List'),
-  ccs: Joi.array().items(emailAddress),
-  bccs: Joi.array().items(emailAddress),
-  subject: Joi.string().required(),
-  content: Joi.string().required(),
-}).label('Mail');
+  tos: Joi.array()
+    .items(emailAddress)
+    .required()
+    .example(['email1@example.com', 'email2@example.com']),
+  ccs: Joi.array().items(emailAddress).example(['email3@example.com', 'email4@example.com']),
+  bccs: Joi.array().items(emailAddress).example(['email5@example.com', 'email6@example.com']),
+  subject: Joi.string().required().description('email subject').example('RE: Sending Email Issue'),
+  content: Joi.string()
+    .required()
+    .description('email text/plain content')
+    .example('Dear Sir/Madam,\n\nThere is a problem in sending email.\n\nRegards,\n\nPerson'),
+}).label('mail');
 
 module.exports = {
   emailAddress,
